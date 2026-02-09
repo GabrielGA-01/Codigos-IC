3098bbc6242d2d61f58a3c151c77d415d524d11c
Gabriel Garcia <gabrielgarcia6508@gmail.com>
2026-02-09 01:04:00 -0300

    Atualizando com os novos hiperparâmetros

--- Codigo/c0_1_configuracoes.py
+++ Codigo/c0_1_configuracoes.py
@@ -52,29 +52,25 @@ param_grid_regressao_logistica_completo = {
 }
 param_grid_perceptron_completo = {
     'classifier__estimator__random_state': [seed],
-    'classifier__estimator__penalty': ['l2', 'l1', 'elasticnet', None],
-    'classifier__estimator__alpha': [0.0001, 0.001, 0.01, 0.1],
-    'classifier__estimator__max_iter': [1000, 2000, 3000],
-    'classifier__estimator__eta0': [0.1, 0.01, 0.001],
+    'classifier__estimator__penalty': [None, 'l2', 'l1', 'elasticnet'],
+    'classifier__estimator__alpha': [0.001, 0.01],
+    'classifier__estimator__max_iter': [1000, 2000],
+    'classifier__estimator__eta0': [0.1, 0.01],
     'classifier__estimator__tol': [1e-3, 1e-4]
 }
 param_grid_random_forest_completo = {
     'classifier__random_state': [seed],
-    'classifier__n_estimators': [50, 100, 200],
-    'classifier__max_depth': [3, 5, 7],
-    'classifier__min_samples_split': [2, 5], 
-    'classifier__min_samples_leaf': [1, 2, 3], 
-    'classifier__criterion': ['gini', 'entropy'],
+    'classifier__n_estimators': [100, 200],
+    'classifier__max_depth': [3, 10],
+    'classifier__min_samples_leaf': [1, 5], 
     'classifier__max_features': ['sqrt', 'log2'] 
 }
 param_grid_xgboost_completo = {
   'classifier__random_state': [seed],
   'classifier__eval_metric': ['logloss'],
-  'classifier__learning_rate': [0.01, 0.05, 0.1],
-  'classifier__max_depth': [3, 5, 7],
-  'classifier__n_estimators': [50, 100, 200],
-  'classifier__subsample': [0.6, 0.8, 1.0],
-  'classifier__colsample_bytree': [0.6, 0.8, 1.0]
+  'classifier__learning_rate': [0.03, 0.05, 0.1],
+  'classifier__max_depth': [3, 5],
+  'classifier__n_estimators': [100, 200]
 }
 
 # Grades básicas (com menos variações ou valores fixos) para testes rápidos ou modelos base.
